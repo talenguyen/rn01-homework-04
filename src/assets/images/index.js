e5dd3014dfb58792e60b112ae866cb752f86faac
Giang Nguyen <giangnguyen.tale@gmail.com>
2021-06-25 08:54:53 +0700

    added images

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,16 @@
 const Images = {
   background: require('./background.png'),
+  card1: require('./card1.png'),
+  card2: require('./card2.png'),
+  card3: require('./card3.png'),
+  card4: require('./card4.png'),
+  card5: require('./card5.png'),
+  card6: require('./card6.png'),
+  card7: require('./card7.png'),
+  card8: require('./card8.png'),
+  card9: require('./card9.png'),
+  card10: require('./card10.png'),
+  cardBack: require('./cardBack.png'),
 };
 
 export default Images;
